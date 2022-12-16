f192bf44ca5a3badedd5e9ece3795cc321f5436d
YousefElzeiny <120676974+YousefElzeiny@users.noreply.github.com>
2022-12-16 15:14:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace project2_perfect_number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number");
+            Console.WriteLine("Enter first number :");
             int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter second number");
+            Console.WriteLine("Enter second number :");
             int n2 = int.Parse(Console.ReadLine());
             for (int i = n1; i <= n2; i++)
             {
